1f460fa8f9b146c68748a42182721faa76394557
yoyoyohidude <zhiqiang.pan@meca-nyc.org>
2024-12-10 18:41:04 +0000

    message

---
+++ final/showMenu.js
@@ -0,0 +1,7 @@
+function showMenu(){
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "none" || menu.style.display ===""){
+        menu.style.display = "block";
+    }else{
+    menu.style.display = "none";}
+}
